1f79060faa7e948ed5ecb0d0ca702db26546a40c
Amr <amraltaweel21@gmail.com>
2026-01-24 15:27:12 -0500

    Did Q2

--- a1/assignment1_AmrImplementation.py
+++ a1/assignment1_AmrImplementation.py
@@ -4,7 +4,7 @@ import math
 
 # A function simulating an artificial neuron
 def artificial_neuron(x, w):
-  # Aggregation Function:SW
+  # Aggregation Function:
   inp = 0
   for i in range(len(x)):
     inp += x[i]*w[i]
@@ -17,13 +17,28 @@ def artificial_neuron(x, w):
 
 # A function performing gradient descent
 def gradient_descent(f, df, x0, alpha):
+  tolerance = 0.0001
+  maxIterations = 1000
   # f is a function that takes as input a list of length n
   # df is the gradient of f; it is a function that takes as input a list of length n
   # x0 is an initial guess for the input minimizing f
   # alpha is the learning rate
+  argmin_f = list(x0)
+
+  for i in range(maxIterations):
+    gradient = df(argmin_f)
+    gradientNorm = 0
+    for j in gradient:
+      gradientNorm += j**2
+    if Math.sqrt(gradientNorm) <= tolerance:
+      break
+
+    for j in range(len(argmin_f)):
+      argmin_f[j] = argmin_f[j] - alpha*gradient[j]
 
   # argmin_f is the input minimizing f
   # min_f is the value of f at its minimum
+  min_f = f(argmin_f)
   return argmin_f, min_f
 
 
