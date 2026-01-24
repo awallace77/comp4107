51b510e2fb17437f50cf05202e74726f7ac70b25
awallace77 <andrew77wallace@outlook.com>
2026-01-24 14:13:13 -0500

    Assignment setups

---
+++ a1/assignment1 (1).py
@@ -0,0 +1,29 @@
+# Name this file assignment1.py when you submit
+import torch
+
+# A function simulating an artificial neuron
+def artificial_neuron(x, w):
+  # x is a list of inputs of length n
+  # w is a list of inputs of length n
+
+  # output is the output from the neuron
+  return output
+
+
+# A function performing gradient descent
+def gradient_descent(f, df, x0, alpha):
+  # f is a function that takes as input a list of length n
+  # df is the gradient of f; it is a function that takes as input a list of length n
+  # x0 is an initial guess for the input minimizing f
+  # alpha is the learning rate
+
+  # argmin_f is the input minimizing f
+  # min_f is the value of f at its minimum
+  return argmin_f, min_f
+
+
+# A function that returns a neural network module in PyTorch
+def pytorch_module():
+
+  # A pytorch module
+  return module
